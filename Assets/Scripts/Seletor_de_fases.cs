fe6fb7d549bac99a87ecfd9c94c8e26b7cbc04e6
joycebrum <joycebrumu.u@gmail.com>
2017-08-30 21:12:05 -0300

    Versão 1.0.1
    Tentei implementar a escolha de fase

---
+++ Assets/Scripts/Seletor_de_fases.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Seletor_de_fases : MonoBehaviour {
+    public string fase;
+   // Use this for initialization
+
+   void OnMouseDown()
+   {
+        Debug.Log("Clicked");
+        SceneManager.LoadScene(fase);
+   }
+}
