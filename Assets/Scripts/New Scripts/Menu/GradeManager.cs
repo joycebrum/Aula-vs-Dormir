7b55c794d99c037ab3912a7e627828c8c949373c
Pedro Nascimento <pedrovitor.marques@gmail.com>
2018-03-18 17:09:17 -0300

    Quase terminando o Sistema de Fases

--- Assets/Scripts/New Scripts/Menu/GradeManager.cs
+++ Assets/Scripts/New Scripts/Menu/GradeManager.cs
@@ -7,7 +7,9 @@ using DG.Tweening;
 public class GradeManager : MonoBehaviour {
     const int semesterTotal = 8;
     [Header("Importants")]
-	public float[,] grade = new float[semesterTotal,3];
+	[HideInInspector]
+    public float[,] grade = new float[semesterTotal,3];
+    [HideInInspector]
     public float[] finalTest = new float[semesterTotal];
 
     [Header("Save Objects")]
@@ -22,8 +24,10 @@ public class GradeManager : MonoBehaviour {
 
     private void Start()
     {
-        grade[0, 0] = 2.3f;
-        grade[0, 1] = 5.4f;
+        InitializeGrade();
+        grade[0, 0] = 4.3f;
+        grade[0, 1] = 5.3f;
+        grade[0, 2] = 1.1f;
         StartUiObjects();
     }
     public void SaveData()
@@ -41,46 +45,113 @@ public class GradeManager : MonoBehaviour {
     }
     public int GetCurrentSemester()
     {
-        for(int i = 0; i < semesterTotal; i++)
-        {
-            if(grade[i,0] == 0)
-            {
-                return i;
-            }
-        }
-        return semesterTotal-1;
+        return PlayerPrefs.GetInt("currentSemester", 1);
     }
     public int GetCurrentTest(int semester)
     {
         for(int i = 0; i < 3; i++)
         {
-            if(grade[semester,i] != 0)
+            if(grade[semester,i] == -1)
             {
                 return i;
             }
         }
-        return 2;
+        return 3;
     }
     public void StartUiObjects()
     {
         //Char Panel
         charPanel.transform.DOScale(new Vector3(0, 0, 0), 0);
         charPanel.transform.DOScale(new Vector3(1, 1, 1), 0.5f);
-        charPanel.transform.GetChild(1).GetComponent<Text>().text = "7.1";
-        charPanel.transform.GetChild(2).GetComponent<Text>().text = "Name";
-        charPanel.transform.GetChild(2).GetChild(0).GetComponent<Text>().text = "2 Semester";
+        charPanel.transform.GetChild(1).GetComponent<Text>().text = GetAverage(GetCurrentSemester() - 1).ToString();
+        charPanel.transform.GetChild(2).GetComponent<Text>().text = "Joyce Brum";
+        charPanel.transform.GetChild(2).GetChild(0).GetComponent<Text>().text = GetCurrentSemester().ToString() + "º Semestre";
         //Class Grid
         CreateClassGrid();
     }
     public void CreateClassGrid()
     {
         string[] names = { "Matematica", "História", "Ciências" };
-        for (int i = 0; i < GetCurrentTest(GetCurrentSemester()); i++){
+        int i = 0;
+        for (i = 0; i < GetCurrentTest(GetCurrentSemester()-1); i++){
             var temp = Instantiate(classUi,Vector3.up,Quaternion.identity,classGrid);
             temp.transform.DOScale(new Vector3(0, 0, 0), 0);
             temp.transform.DOScale(new Vector3(1, 1, 1), 0.5f);
             temp.transform.GetChild(0).GetComponent<Text>().text = names[i];
-            temp.transform.GetChild(1).GetChild(1).GetComponent<Text>().text = "7";
+            temp.transform.GetChild(1).GetChild(1).GetComponent<Text>().text = grade[GetCurrentSemester()-1,i].ToString();
         }
+        if(i == 3)
+        {
+            if(GetAverage(GetCurrentSemester() - 1) < 5.0)
+            {
+                ShowFinalTest(GetCurrentSemester() - 1);
+                if (finalTest[GetCurrentSemester()-1] != -1 && GetAverageWithFinalTest(GetCurrentSemester()-1) < 5.0)
+                {
+                    print("REPROVADO");
+                }
+                else if(finalTest[GetCurrentSemester() - 1] != -1)
+                {
+                    print("PASSAR DE SEMESTRE COM FINAL");
+                }
+            }
+            else
+            {
+                finalTest[GetCurrentSemester() - 1] = 0;
+                print("PASSAR DE SEMESTRE");
+            }
+        }
+    }
+    public float GetAverage(int semester)
+    {
+        if(grade[semester, 0] != -1 && grade[semester, 1] != -1 && grade[semester, 2] != -1)
+        {
+            return (grade[semester, 0] + grade[semester, 1] + grade[semester, 2]) / 3;
+        }
+        return 0;
+    }
+    public float GetAverageWithFinalTest(int semester)
+    {
+        return (GetAverage(semester) + finalTest[semester]) / 2;
+    }
+    public void InitializeGrade()
+    {
+        for(int i = 0; i < 3; i++)
+        {
+            for(int j = 0; j < semesterTotal; j++)
+            {
+                grade[j,i] = -1;
+            }
+            finalTest[i] = -1;
+        }
+    }
+    public void ShowFinalTest(int semester)
+    {
+        var temp = Instantiate(finalClassUi, Vector3.up, Quaternion.identity, classGrid);
+        temp.transform.DOScale(new Vector3(0, 0, 0), 0);
+        temp.transform.DOScale(new Vector3(1, 1, 1), 1f);
+        temp.transform.GetChild(0).GetComponent<Text>().text = "Prova Final";
+
+        if (finalTest[semester] == -1)
+        {
+            temp.transform.GetChild(1).GetChild(1).GetComponent<Text>().text = "??";
+        }
+        else
+        {
+            temp.transform.GetChild(1).GetChild(1).GetComponent<Text>().text = finalTest[semester].ToString();
+        }
+    }
+    public void NextSemester()
+    {
+        PlayerPrefs.SetInt("currentSemester",GetCurrentSemester()+1);
+        PlayerPrefs.Save();
+    }
+    public void ResetSemester()
+    {
+        PlayerPrefs.SetInt("currentSemester", 1);
+        PlayerPrefs.Save();
+    }
+    public void StartLevel(int i)
+    {
+        print(i);
     }
 }
